689eb8bd73066274bfb733013367ddfe40e93bca
Joe Zhou <zhoushuqun@gmail.com>
2015-01-31 02:02:27 +0800

    Update peatio website.

--- peatio-client-rest/src/main/java/org/oxerr/peatio/rest/package-info.java
+++ peatio-client-rest/src/main/java/org/oxerr/peatio/rest/package-info.java
@@ -1,4 +1,4 @@
 /**
- * Client for <a href="http://peatio.com">Peatio</a> RESTful API.
+ * Client for <a href="http://peat.io">Peatio</a> RESTful API.
  */
 package org.oxerr.peatio.rest;
--- peatio-client-websocket/src/main/java/org/oxerr/peatio/websocket/package-info.java
+++ peatio-client-websocket/src/main/java/org/oxerr/peatio/websocket/package-info.java
@@ -1,4 +1,4 @@
 /**
- * Client for <a href="http://peatio.com">Peatio</a> WebSocket API.
+ * Client for <a href="http://peat.io">Peatio</a> WebSocket API.
  */
 package org.oxerr.peatio.websocket;
